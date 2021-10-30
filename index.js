3b3a53b931b7ce8d71b773a40dc5364abdaca9cb
DruboSaptapan <saptapan2017@gmail.com>
2021-10-31 02:24:19 +0600

    server updated

--- index.js
+++ index.js
@@ -1,74 +1,181 @@
+// const express = require('express');
+// const { MongoClient } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectId;
+// const cors = require('cors');
+// require('dotenv').config()
+
+// const app = express();
+// const port = process.env.PORT || 7000;
+
+// // MiddleWare
+// app.use(cors());
+// app.use(express.json());
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b5wnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// async function run() {
+//     try {
+//         await client.connect();
+//         const database = client.db("heavenPeace")
+//         const packagesCollection = database.collection("packages")
+//         const ordersCollection = database.collection("orders")
+
+//         // GET Products API
+//         app.get('/packages', async (req, res) => {
+//             const cursor = packagesCollection.find({});
+//             const packages = await cursor.toArray();
+//             res.send(packages);
+//         })
+
+//         // POST API
+//         app.post('/packages', async (req, res) => {
+//             const package = req.body;
+//             const result = await packagesCollection.insertOne(package);
+//             res.json(result);
+//         })
+
+//         // Add Orders API
+//         app.post('/orders', async (req, res) => {
+//             const order = req.body;
+//             const result = await ordersCollection.insertOne(order);
+//             res.json(result);
+//         })
+
+//         // GET ORDER API
+//         app.get('/orders', async (req, res) => {
+//             const cursor = ordersCollection.find({});
+//             const orders = await cursor.toArray();
+//             res.send(orders);
+//         })
+
+//         // Manage Users
+//         // app.get('/orders/:id', async (req, res) => {
+//         //     const id = req.params.id;
+//         //     const query = { _id: ObjectId(id) };
+//         //     const order = await orderCollection.findOne(query);
+//         //     res.json(order);
+//         // })
+
+
+//         // DELETE API
+//         app.delete('/orders/:id', async (req, res) => {
+//             const id = req.params.id;
+//             const query = { _id: ObjectId(id) };
+//             const result = await ordersCollection.deleteOne(query);
+//             res.json(result);
+//         })
+
+//         // Get My Orders
+//         // app.get('/order/:email', async (req, res) => {
+//         //     const email = req.params.email;
+//         //     const query = { "email": email };
+//         //     const order = await ordersCollection.find(query);
+//         //     res.json(order);
+//         // })
+
+//     }
+//     finally {
+//         // await client.close();
+//     }
+// }
+// run().catch(console.dir);
+
+
+// app.get('/', (req, res) => {
+//     res.send('Wonder Travel node mongo server')
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+
+
 const express = require('express');
-const { MongoClient } = require("mongodb")
-const objectId = require('mongodb').ObjectId;
-
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
-
-require('dotenv').config();
+require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
-// middleware
+// MiddleWare
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b5wnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri);
 
 async function run() {
     try {
+
+        // Connect to database
         await client.connect();
-        const database = client.db('heavenPeace')
-        const servicesCollection = database.collection('services');
-        console.log('Successfully database connected.')
+        const database = client.db("heavenPeace")
+        const packagesCollection = database.collection("packages")
+        const ordersCollection = database.collection("orders")
 
-        // get api
-        app.get('/services', async (req, res) => {
-            const cursor = servicesCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
+        // Get Packages API
+        app.get('/packages', async (req, res) => {
+            const cursor = packagesCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
         })
 
-        // get single service
-        app.get('/services/:id', async (req, res) => {
+        // Add Packages API
+        app.post('/packages', async (req, res) => {
+            const package = req.body;
+            const result = await packagesCollection.insertOne(package);
+            res.json(result);
+        })
+
+        // Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        })
+
+        // Get Orders API
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        // Delete Orders API
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            console.log('getting specific service.', id)
-            const query = { _id: objectId(id) };
-            const service = await servicesCollection.findOne(query)
-            res.json(service);
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
         })
 
-        // post api
-        app.post('/services', async (req, res) => {
-            const service = req.body;
-            console.log('Hit the post api', service)
-
-            const result = await servicesCollection.insertOne(service);
-            console.log(result);
-            res.json(result)
-        })
-
-        // DELETE API heavenPeace services
-        app.delete('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id:objectId(id) }
-            const result = await servicesCollection.deleteOne(query)
-            res.json(result)
+        // Get MyOrders API
+        app.get('/order/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { "email": email };
+            const order = await ordersCollection.find(query);
+            res.json(order);
         })
     }
     finally {
-        // await client.close()
+        // await client.close();
     }
 }
-
 run().catch(console.dir);
 
+
 app.get('/', (req, res) => {
-    res.send('Welcome to heaven peace website server');
+    res.send('Welcome to Heaven Peace server')
 });
 
+
 app.listen(port, () => {
-    console.log(`I'm listening at ${port}`);
+    console.log(`I'm listening to ${port}`)
 })
